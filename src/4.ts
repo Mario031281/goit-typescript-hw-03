0fa2a4d8cf7f5b5762c383c9a1e59345c0219de2
Mario031281 <taracatus@gmail.com>
2023-10-15 19:33:25 +0300

    erst

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,48 @@
+class Key {
+  constructor(private signature: number = Math.random()) {}
+
+  getSignature() {
+    return this.signature;
+  }
+}
+class MyHouse {
+  private locked: boolean = true;
+  private key: Key | null;
+
+  constructor(key: Key | null) {
+    this.key = key;
+  }
+
+  openDoor(key: Key | null): void {
+    if (key === this.key) {
+      this.locked = false;
+      console.log("Door unlocked.");
+    } else {
+      console.log("The key doesn't fit. The doors remain closed.");
+    }
+  }
+
+  comeIn(person: Person): void {
+    if (!this.locked) {
+      console.log(`${person.constructor.name} enters the House.`);
+    } else {
+      console.log("The door is closed. You can't log in.");
+    }
+  }
+}
+
+class Person {
+  private key: Key | null;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key | null {
+    return this.key;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +51,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
